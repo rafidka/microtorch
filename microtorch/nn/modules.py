11abb4fac4f54db807845c3d3ac2f07f39011f4f
Rafid <rafidka@gmail.com>
2024-12-15 21:45:01 +0000

    Pyright, ruff, GitHub actions, and small code changes

--- microtorch/nn/modules.py
+++ microtorch/nn/modules.py
@@ -1,47 +1,44 @@
 # 3rd party imports
+from typing import Any
 import numpy as np
 
 # Local imports
 from microtorch.tensor import Tensor, functional as F
 
 
-class Module:
+class Module[T]:
     def __init__(self):
-        self._modules = {}
+        self._modules: dict[str, "Module[T]"] = {}
 
-    def add_module(self, name, module):
+    def add_module(self, name: str, module: "Module[T]"):
         self._modules[name] = module
 
-    def __setattr__(self, name, value):
+    def __setattr__(self, name: str, value: Any):
         if isinstance(value, Module):
-            self.add_module(name, value)
+            self.add_module(name, value)  # type: ignore
         super(Module, self).__setattr__(name, value)
 
-    def __getattr__(self, name):
-        if name in self._modules:
-            return self._modules[name]
-        return super(Module, self).__getattr__(name)
-
-    def __call__(self, *args, **kwargs):
+    def __call__(self, *args: Any, **kwargs: Any):
         return self.forward(*args, **kwargs)
 
-    def forward(self):
+    def forward(self, *args: Any, **kwargs: Any) -> T:
         raise NotImplementedError
 
 
-class Linear(Module):
-    def __init__(self, in_features, out_features):
+class Linear(Module[Tensor]):
+    def __init__(self, in_features: int, out_features: int):
         super(Linear, self).__init__()
         self.weight = Tensor(
             np.random.randn(in_features, out_features), requires_grad=True
         )
         self.bias = Tensor(np.zeros(out_features), requires_grad=True)
 
-    def forward(self, x):
+    def forward(self, x: Tensor) -> Tensor:
         return x @ self.weight + self.bias
 
 
-class Softmax(Module):
-    def forward(self, x):
+class Softmax(Module[Tensor]):
+    def forward(self, x: Tensor) -> Tensor:
         exps = F.exp(x - F.max(x, axis=1, keepdims=True))
-        return exps / sum(exps, axis=1, keepdims=True)
+        s = F.sum(exps, axis=1, keepdims=True)
+        return exps / s
