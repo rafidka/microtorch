82b0ffdaed5def0f2bde08b54150fbbe471b92a8
Rafid <rafidka@gmail.com>
2025-03-02 22:29:00 +0000

    Expose CrossEntropyLoss in microtorch.nn module

--- microtorch/nn/__init__.py
+++ microtorch/nn/__init__.py
@@ -1,11 +1,13 @@
 from .modules.activation import ReLU, Softmax
 from .modules.linear import Linear
+from .modules.loss import CrossEntropyLoss
 from .modules.module import Module
 from .modules.parameter import Parameter
 
 __all__ = [
-    "Module",
+    "CrossEntropyLoss",
     "Linear",
+    "Module",
     "Parameter",
     "ReLU",
     "Softmax",
