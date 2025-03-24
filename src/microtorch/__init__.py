53b36daf351a0dbde9b0b9b856bcff9eb5f30c6f
Rafid <rafidka@gmail.com>
2025-03-24 01:10:49 +0000

    Method equal() for comparing tensors

--- src/microtorch/__init__.py
+++ src/microtorch/__init__.py
@@ -1,5 +1,22 @@
 __version__ = "0.1.0"
 
+import numpy as np
+
 from . import tensor
 
-__all__ = ["tensor"]
+
+def equal(a: tensor.Tensor, b: tensor.Tensor) -> bool:
+    """
+    Check if two tensors are element-wise equal.
+
+    Args:
+        a (tensor.Tensor): The first tensor to compare.
+        b (tensor.Tensor): The second tensor to compare.
+
+    Returns:
+        bool: True if the tensors are element-wise equal, False otherwise.
+    """
+    return np.equal(a._data, b._data).all()
+
+
+__all__ = ["tensor", "equal"]
